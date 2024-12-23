86dd670e9a0719f303061fbe9a8c9513f5d47762
kanti-lal <kanti.ylocation@gmail.com>
2024-12-24 01:32:31 +0530

    fix:build issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
